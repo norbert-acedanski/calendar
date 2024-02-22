44d1afd38ac0e9eaf367fb2778564b4465566aeb
Norbert Acedanski <norbert.acedanski@gmail.com>
2024-02-22 15:52:52 +0100

    Add base classes and Holidays class with proper entries.

--- holidays.py
+++ holidays.py
@@ -1,5 +1,8 @@
 import datetime
+import json
 import requests
+import warnings
+from typing import Any, Dict, Generator, List
 
 from config import YEAR
 
@@ -21,3 +24,85 @@ CHRISTMAS_DAY_2 = datetime.datetime(year=YEAR, month=12, day=26)
 NATIONAL_DAYS_OFF = [NEW_YEAR, EPIPHANY, EASTER_DAY, EASTER_MONDAY, LABOUR_DAY, CONSTITUTION_DAY, PENTECOST,
                      CORPUS_CHRISTI_DAY, ASSUMPTION_OF_THE_BLESSED_VIRGIN_MARY_DAY, ALL_SAINTS_DAY, INDEPENDENCE_DAY,
                      CHRISTMAS_DAY_1, CHRISTMAS_DAY_2]
+
+
+class GenericDefaults:
+    @classmethod
+    def get_in_order(cls) -> Generator[Any, None, None]:
+        yield from (val for key, val in cls.__dict__.items() if not key.startswith("__"))
+
+
+class BaseClass:
+    @staticmethod
+    def create_days_off(days_off: List[Dict[str, Dict[str, int]]],
+                        include_weekends: bool = False) -> List[datetime.datetime]:
+        dates = []
+        for days_off_range in days_off:
+            start_day = days_off_range.get("start", days_off_range.get("single_day"))
+            end_day = days_off_range.get("end", days_off_range.get("single_day"))
+            delta = datetime.datetime(year=YEAR, month=end_day["month"], day=end_day["day"]) - \
+                                     (start_day := datetime.datetime(year=YEAR, month=start_day["month"],
+                                                                     day=start_day["day"]))
+            dates += [start_day + datetime.timedelta(i) for i in range(delta.days + 1)
+                      if (not (start_day + datetime.timedelta(i)).weekday() in [5, 6] or include_weekends)
+                      and not start_day + datetime.timedelta(i) in NATIONAL_DAYS_OFF]
+        return dates
+
+    @staticmethod
+    def load_json_data(file_name: str, holiday_category: str) -> List[Dict[str, Dict[str, int]]]:
+        try:
+            with open(file=file_name, mode="r") as json_file:
+                content = json.load(json_file)
+        except FileNotFoundError:
+            content = {}
+        return content.get(holiday_category, [])
+
+
+class Holidays(GenericDefaults):
+    class NationalHolidays:
+        name = "NATIONAL HOLIDAYS"
+        days_off = NATIONAL_DAYS_OFF
+        color = "#D50A20"
+
+    class NationalHolidaysOnSaturdayReplacementDays:
+        name = "NATIONAL HOLIDAYS ON SATURDAY REPLACEMENT DAYS"
+        days_off = BaseClass.load_json_data(file_name=f"input_files/calendar_{YEAR}.json", holiday_category=name)
+        days_off = BaseClass.create_days_off(days_off=days_off)  # [datetime.datetime(year=YEAR, month=3, day=29)]
+        color = "#00A2E8"
+
+    class PlasmaDonationHolidays:
+        name = "PLASMA DONATION"
+        days_off = BaseClass.load_json_data(file_name=f"input_files/calendar_{YEAR}.json", holiday_category=name)
+        days_off = BaseClass.create_days_off(days_off=days_off)
+        color = "#A349A4"
+
+    class BloodDonationHolidays:
+        name = "BLOOD DONATION"
+        days_off = BaseClass.load_json_data(file_name=f"input_files/calendar_{YEAR}.json", holiday_category=name)
+        days_off = BaseClass.create_days_off(days_off=days_off)
+        color = "#22B14C"
+
+    class Holidays:
+        name = "HOLIDAYS"
+        days_off = BaseClass.load_json_data(file_name=f"input_files/calendar_{YEAR}.json", holiday_category=name)
+        days_off = BaseClass.create_days_off(days_off=days_off)
+        color = "#FF7F27"
+
+    class SpecialOccasions:
+        name = "SPECIAL OCCASIONS HOLIDAYS"
+        days_off = BaseClass.load_json_data(file_name=f"input_files/calendar_{YEAR}.json", holiday_category=name)
+        days_off = BaseClass.create_days_off(days_off=days_off)
+        color = "#FFAECC"
+
+    class Childcare:
+        name = "CHILDCARE"
+        days_off = BaseClass.load_json_data(file_name=f"input_files/calendar_{YEAR}.json", holiday_category=name)
+        days_off = BaseClass.create_days_off(days_off=days_off)
+        color = "#3F48CC"
+
+
+if len((saturday_holidays := [national_holiday for national_holiday in Holidays.NationalHolidays.days_off
+                              if national_holiday.weekday() == 5])) \
+        != len(Holidays.NationalHolidaysOnSaturdayReplacementDays.days_off):
+    warnings.warn(f"Please input all days, that are a replacement for the Holidays,that are on saturday! "
+                  f"{saturday_holidays}")
