7320c79a76b94ba99c21e65438d5480ad1b8864c
Norbert Acedanski <norbert.acedanski@gmail.com>
2024-02-23 10:42:45 +0100

    Move National Days off to a separate class.

--- holidays.py
+++ holidays.py
@@ -6,24 +6,26 @@ from typing import Any, Dict, Generator, List
 
 from config import YEAR
 
-NEW_YEAR = datetime.datetime(year=YEAR, month=1, day=1)
-EPIPHANY = datetime.datetime(year=YEAR, month=1, day=6)
-easter_in_selected_year = requests.get(f"https://www.calendardate.com/easter_{YEAR}.htm",
-                                       verify=False).text.split(f"Easter {YEAR} is on")[1].split(", ")[1]
-EASTER_DAY = datetime.datetime.strptime(f"{YEAR} {easter_in_selected_year}", '%Y %B %d')
-EASTER_MONDAY = EASTER_DAY + datetime.timedelta(days=1)
-LABOUR_DAY = datetime.datetime(year=YEAR, month=5, day=1)
-CONSTITUTION_DAY = datetime.datetime(year=YEAR, month=5, day=3)
-PENTECOST = EASTER_DAY + datetime.timedelta(weeks=7)
-CORPUS_CHRISTI_DAY = EASTER_DAY + datetime.timedelta(days=60)
-ASSUMPTION_OF_THE_BLESSED_VIRGIN_MARY_DAY = datetime.datetime(year=YEAR, month=8, day=15)
-ALL_SAINTS_DAY = datetime.datetime(year=YEAR, month=11, day=1)
-INDEPENDENCE_DAY = datetime.datetime(year=YEAR, month=11, day=11)
-CHRISTMAS_DAY_1 = datetime.datetime(year=YEAR, month=12, day=25)
-CHRISTMAS_DAY_2 = datetime.datetime(year=YEAR, month=12, day=26)
-NATIONAL_DAYS_OFF = [NEW_YEAR, EPIPHANY, EASTER_DAY, EASTER_MONDAY, LABOUR_DAY, CONSTITUTION_DAY, PENTECOST,
-                     CORPUS_CHRISTI_DAY, ASSUMPTION_OF_THE_BLESSED_VIRGIN_MARY_DAY, ALL_SAINTS_DAY, INDEPENDENCE_DAY,
-                     CHRISTMAS_DAY_1, CHRISTMAS_DAY_2]
+
+class NationalDaysOff:
+    NEW_YEAR = datetime.datetime(year=YEAR, month=1, day=1)
+    EPIPHANY = datetime.datetime(year=YEAR, month=1, day=6)
+    easter_in_selected_year = requests.get(f"https://www.calendardate.com/easter_{YEAR}.htm",
+                                           verify=False).text.split(f"Easter {YEAR} is on")[1].split(", ")[1]
+    EASTER_DAY = datetime.datetime.strptime(f"{YEAR} {easter_in_selected_year}", '%Y %B %d')
+    EASTER_MONDAY = EASTER_DAY + datetime.timedelta(days=1)
+    LABOUR_DAY = datetime.datetime(year=YEAR, month=5, day=1)
+    CONSTITUTION_DAY = datetime.datetime(year=YEAR, month=5, day=3)
+    PENTECOST = EASTER_DAY + datetime.timedelta(weeks=7)
+    CORPUS_CHRISTI_DAY = EASTER_DAY + datetime.timedelta(days=60)
+    ASSUMPTION_OF_THE_BLESSED_VIRGIN_MARY_DAY = datetime.datetime(year=YEAR, month=8, day=15)
+    ALL_SAINTS_DAY = datetime.datetime(year=YEAR, month=11, day=1)
+    INDEPENDENCE_DAY = datetime.datetime(year=YEAR, month=11, day=11)
+    CHRISTMAS_DAY_1 = datetime.datetime(year=YEAR, month=12, day=25)
+    CHRISTMAS_DAY_2 = datetime.datetime(year=YEAR, month=12, day=26)
+    NATIONAL_DAYS_OFF = [NEW_YEAR, EPIPHANY, EASTER_DAY, EASTER_MONDAY, LABOUR_DAY, CONSTITUTION_DAY, PENTECOST,
+                         CORPUS_CHRISTI_DAY, ASSUMPTION_OF_THE_BLESSED_VIRGIN_MARY_DAY, ALL_SAINTS_DAY, INDEPENDENCE_DAY,
+                         CHRISTMAS_DAY_1, CHRISTMAS_DAY_2]
 
 
 class GenericDefaults:
@@ -45,7 +47,7 @@ class BaseClass:
                                                                      day=start_day["day"]))
             dates += [start_day + datetime.timedelta(i) for i in range(delta.days + 1)
                       if (not (start_day + datetime.timedelta(i)).weekday() in [5, 6] or include_weekends)
-                      and not start_day + datetime.timedelta(i) in NATIONAL_DAYS_OFF]
+                      and not start_day + datetime.timedelta(i) in NationalDaysOff.NATIONAL_DAYS_OFF]
         return dates
 
     @staticmethod
@@ -61,7 +63,7 @@ class BaseClass:
 class Holidays(GenericDefaults):
     class NationalHolidays:
         name = "NATIONAL HOLIDAYS"
-        days_off = NATIONAL_DAYS_OFF
+        days_off = NationalDaysOff.NATIONAL_DAYS_OFF
         color = "#D50A20"
 
     class NationalHolidaysOnSaturdayReplacementDays:
