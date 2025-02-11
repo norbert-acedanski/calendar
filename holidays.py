4e12c2d2fc8d311b3d5437305829ece75f9fe473
Norbert Acedanski <norbert.acedanski@gmail.com>
2025-02-11 16:03:49 +0100

    Add Christmas Eve as another holiday.

--- holidays.py
+++ holidays.py
@@ -20,11 +20,12 @@ class NationalDaysOff:
     ASSUMPTION_OF_THE_BLESSED_VIRGIN_MARY_DAY = datetime.datetime(year=YEAR, month=8, day=15)
     ALL_SAINTS_DAY = datetime.datetime(year=YEAR, month=11, day=1)
     INDEPENDENCE_DAY = datetime.datetime(year=YEAR, month=11, day=11)
+    CHRISTMAS_EVE = datetime.datetime(year=YEAR, month=12, day=24)
     CHRISTMAS_DAY_1 = datetime.datetime(year=YEAR, month=12, day=25)
     CHRISTMAS_DAY_2 = datetime.datetime(year=YEAR, month=12, day=26)
     NATIONAL_DAYS_OFF = [NEW_YEAR, EPIPHANY, EASTER_DAY, EASTER_MONDAY, LABOUR_DAY, CONSTITUTION_DAY, PENTECOST,
                          CORPUS_CHRISTI_DAY, ASSUMPTION_OF_THE_BLESSED_VIRGIN_MARY_DAY, ALL_SAINTS_DAY, INDEPENDENCE_DAY,
-                         CHRISTMAS_DAY_1, CHRISTMAS_DAY_2]
+                         CHRISTMAS_EVE, CHRISTMAS_DAY_1, CHRISTMAS_DAY_2]
 
 
 class Holidays(GenericDefaults):
